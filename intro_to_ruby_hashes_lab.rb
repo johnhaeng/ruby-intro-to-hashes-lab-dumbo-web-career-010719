574c00dc3bac78f07ec80b4d9dc8f75242e9fc87
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-16 06:11:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,9 +21,8 @@ def monopoly_with_second_tier
 	  
 	  monopoly = {
 	               railroads: {
-	                          pieces: 4
-	                          names: 
-	                                {
+	                pieces: 4
+	                names: {
 	                    
 	                                }
 	                        }
